5c398f66dca83224d7eda31f750225f32201bf6e
Shiva Chaitanya <shivachaitanya707@gmail.com>
2024-07-17 10:34:05 +0530

    Initial commit

--- js/client.js
+++ js/client.js
@@ -1,4 +1,6 @@
-const socket= io('https://localhost:8000');
+// const socket= io('https://localhost:8000');
+// const socket = io("http://localhost:8000");
+const socket = io("http://localhost:8000", { transports: ["websocket"] });
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,21 +1,25 @@
-//Node servver which will handle socket io connections
-const io= require('socket.io')(8000)
+const io = require('socket.io')(8000, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 const users = {};
 
-io.on('connection',socket=>{
-    socket.on('new-user-joined',userName=>{
-           console.log("New user",userName);
-           users[socket.id]=userName;
-           socket.broadcast.emit('user-joined',userName);
+io.on('connection', socket => {
+    socket.on('new-user-joined', userName => {
+        console.log("New user", userName);
+        users[socket.id] = userName;
+        socket.broadcast.emit('user-joined', userName);
     });
 
-    socket.on('send',message=>{
-        socket.broadcast.emit('receive',{message:message,name: users[socket.id]})
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
     });
 
-    socket.on('disconnect',message=>{
-        socket.broadcast.emit('left',users[socket.id]);
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
-})
+});
